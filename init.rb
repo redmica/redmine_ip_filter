2d7e6ef84a2b2fbac598ac9002341cba98f38aaf
MAEDA Go <maeda@farend.jp>
2020-07-20 22:30:33 +0900

    Update plugin attribute (#9)

--- init.rb
+++ init.rb
@@ -2,11 +2,11 @@ require_dependency 'application_controller_patch'
 require_dependency 'redmine_ip_filter_hook_listener'
 Redmine::Plugin.register :redmine_ip_filter do
   name 'Redmine Ip Filter'
-  author 'Farend Technologies Corporation'
+  author 'Far End Technologies Corporation'
   description 'Redmine plugin for access filtering using IP address.'
   version '0.0.1'
   url 'http://github.com/redmica/redmine_ip_filter'
-  author_url 'http://www.farend.co.jp/'
+  author_url 'https://hosting.redmine.jp/'
   menu :admin_menu, :redmine_ip_filter, { controller: :filter_rules, action: :edit }, caption: :label_ip_filter, :html => { :class => 'icon icon-ip-filter' }
   settings :default => { :allowed_ips => '' }
 end
