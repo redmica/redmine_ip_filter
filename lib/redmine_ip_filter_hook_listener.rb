26d1ec2656ef45b1aae663d4956551021472eb7b
MAEDA Go <maeda@farend.jp>
2020-07-22 18:23:42 +0900

    Enable frozen_string_literal

--- lib/redmine_ip_filter_hook_listener.rb
+++ lib/redmine_ip_filter_hook_listener.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RedmineIpFilterHookListener < Redmine::Hook::ViewListener
   def view_layouts_base_html_head(context)
     stylesheet_link_tag 'redmine_ip_filter', plugin: :redmine_ip_filter
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Load the Redmine helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 ActiveRecord::FixtureSet.create_fixtures(File.dirname(__FILE__) + '/fixtures', 'filter_rules')
